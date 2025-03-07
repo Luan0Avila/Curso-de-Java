e407cb780fc2c1911a760082c51a5d379d9d4a53
Luan0Avila <avila.almeida.luan@gmail.com>
2025-03-07 13:04:27 -0300

    terminando os exercicios e aprendendo um pouco sobre mais condicionais

---
+++ secao3/Condicionias2.java
@@ -0,0 +1,13 @@
+package secao3;
+
+public class Condicionias2 {
+    public static void main(String[] args) {
+    
+        // 1 - Condicional ternária
+        int numero = 10;
+
+        String resultado = (numero % 2 == 0) ? "par" : "impar";
+
+        System.out.println(resultado);
+    }
+}
--- secao3/Exercicios.java
+++ secao3/Exercicios.java
@@ -8,7 +8,8 @@ public class Exercicios {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Digite o preço de um produto: ");
-        double preco = scanner.nextInt();
+
+        double preco = scanner.nextDouble();
 
 
         if (preco < 50) {
@@ -19,6 +20,110 @@ public class Exercicios {
             System.out.println("Médio");
         } else {
             System.out.println("Caro");
+        }
+
+        scanner.nextLine();
+
+
+        // Exercicio 2
+        System.out.print("Digite seu nome de usuário: ");
+        
+        String userName = scanner.next();
+
+        System.out.print("Digite sua senha: ");
+
+        String senha = scanner.next();
+
+        if (userName.equals("admin") && senha.equals("1234")) {
+        
+            System.out.println("Acesso permitido");
+        
+        } else {
+
+            System.out.println("Acesso negado");
+
+        }
+
+
+        // Exercicio 3
+        System.out.print("Digite um numero: ");
+
+        int numero = scanner.nextInt();
+        
+        String resultado = "";
+
+        if (numero % 2 == 0) {
+
+            resultado = "Par";
+            
+        } else {
+
+            resultado = "Impar";
+        }
+
+        System.out.println(resultado);
+
+
+        //Exercicio 4
+        System.out.println("Digite um numero de 1 a 7 (Referente a um dia da semana)");
+
+        int dia = scanner.nextInt();
+
+        switch (dia) {
+            case 2, 3, 4, 5, 6:
+                
+            System.out.println("Dia útil");
+
+                break;
+        
+            case 1, 7:
+
+            System.out.println("Final de semana");
+
+                break;
+    
+            default:
+
+            System.out.println("não é um dia válido");
+
+                break;
+            
+        }
+
+
+        // Exercicio 5
+        System.out.println("Digite um numero: ");
+
+        int num = scanner.nextInt();
+
+        if (num >= 10 && num <= 20) {
+
+            System.out.println("Dentro do intervalo");
+        
+        } else {
+            System.out.println("Fora do intervalo");
+        }
+
+        scanner.nextLine();
+
+
+        // Exericio 6
+        System.out.println("Digite uma letra: ");
+
+        char letra = scanner.next().toLowerCase().charAt(0);
+
+        switch (letra) {
+            case 'a', 'e', 'i', 'o', 'u':
+
+            System.out.println("Vogal");
+                
+                break;
+    
+            default:
+    
+            System.out.println("Consoante");
+
+            break;
         }
 
         scanner.close();
