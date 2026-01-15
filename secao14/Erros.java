a94161cf106a248891511544649cd47e37d165aa
Luan0AVila <avila.almeida.luan@gmail.com>
2026-01-15 12:43:24 -0300

    aprendendo sobre o bloco finally

--- secao14/Erros.java
+++ secao14/Erros.java
@@ -25,8 +25,49 @@ public class Erros {
             System.out.println("Erro genérico");
             
             System.out.println("Msg:" + e.getMessage());
+            // Excessão e erro => para o mesmo fim
         }
-        // Excessão e erro => para o mesmo fim
 
+        // 2 - finally
+        try {
+            int[] numeros = {1,2,3};
+
+            System.out.println(numeros[3]);
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Erro genérico finally");
+            
+            System.out.println("Msg:" + e.getMessage());
+        } finally {
+            System.out.println("Executou o finally");
+        }
+
+
+
+        try {
+            int[] numeros = {1,2,3};
+
+            System.out.println("Acessando indice existente");
+            System.out.println(numeros[2]);
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Erro genérico finally");
+            
+            System.out.println("Msg:" + e.getMessage());
+        } finally {
+            System.out.println("Executou o finally");
+        }
+
+
+
+        try {
+            int[] numeros = {1,2,3};
+
+            System.out.println("Acessando indice sem catch");
+            System.out.println(numeros[2]);
+
+        } finally {
+            System.out.println("Executou o finally");
+        }
     }
 }
