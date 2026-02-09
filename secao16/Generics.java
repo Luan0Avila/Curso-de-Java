c387ea1b47d5eea0c41e6f6e01b74f878dd370b4
Luan0Avila <avila.almeida.luan@gmail.com>
2026-02-08 21:03:50 -0300

    aprendendo a implementar métodos genericos

--- secao16/Generics.java
+++ secao16/Generics.java
@@ -1,5 +1,7 @@
 package secao16;
 
+import java.util.*;
+
 public class Generics {
     
     public static void main(String[] args) {
@@ -21,5 +23,30 @@ public class Generics {
         caixaStr.adicionar("teste");
 
                 System.out.println(caixaStr.obter());
+
+        // 2 - métodos genericos
+        System.out.println("Maior valor entre 5 ew 10 " + obterMaior(5, 10));
+        System.out.println("Maior valor entre Java e Python " + obterMaior("Java", "Python"));
+
+        String[] letras = {"a", "b", "c", "d"};
+
+        Integer[] numeros = {11, 17, 8, 0, 10};
+
+        Boolean[] bools = {true, false, false, true};
+        
+        imprimirArray(letras);
+        imprimirArray(numeros);
+        imprimirArray(bools);
+        
+    }
+
+    public static <T extends Comparable<T>> T obterMaior(T valor1, T valor2) {
+        return (valor1.compareTo(valor2) > 0) ? valor1 : valor2;
+    }
+
+    public static<T> void imprimirArray(T[] array) {
+        for(T elemento : array) {
+            System.out.print(elemento + ", ");
+        }
     }
 }
