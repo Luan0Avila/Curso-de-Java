6428cdf5475a75b8ea720b26b48a9213063fa19c
Luan0Avila <avila.almeida.luan@gmail.com>
2026-05-16 14:08:24 -0300

    executtando o primeiro programa com javaFX

---
+++ secao20/HelloWorldFX/src/helloworldfx/Controller.java
@@ -0,0 +1,17 @@
+package helloworldfx;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class Controller {
+
+    @FXML
+    private Label label;
+
+    public void initialize() {
+        String javaVersion = System.getProperty("java.version");
+        String javafxVersion = System.getProperty("javafx.version");
+        label.setText("Olá mundo JavaFX");
+    }
+}
+
