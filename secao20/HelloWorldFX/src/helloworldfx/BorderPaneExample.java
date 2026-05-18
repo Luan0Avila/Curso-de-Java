79d51ec4eb3e81f08ba57d6861f4e2aa16b0f650
Luan0Avila <avila.almeida.luan@gmail.com>
2026-05-17 23:08:50 -0300

    aprendendo sobre o borderPane

--- secao20/HelloWorldFX/src/helloworldfx/BorderPaneExample.java
+++ secao20/HelloWorldFX/src/helloworldfx/BorderPaneExample.java
@@ -1,12 +1,11 @@
 package helloworldfx;
-
 import javafx.application.*;
 import javafx.scene.*;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.stage.*;
 
-public class VboxHboxExample extends Application{
+public class BorderPaneExample extends Application{
     @Override
     public void start(Stage primaryStage) throws Exception{
 
@@ -14,24 +13,25 @@ public class VboxHboxExample extends Application{
         Button btn1 = new Button("Botão1");
         Button btn2 = new Button("Botão2");
         Button btn3 = new Button("Botão3");
-
-        VBox vBox = new VBox(15);
-        vBox.getChildren().addAll(btn1,btn2,btn3);
-
-        VBox root = new VBox(20);
-        
-        // criar alguns botões hbox
         Button btn4 = new Button("Botão4");
         Button btn5 = new Button("Botão5");
         Button btn6 = new Button("Botão6");
-        
-        HBox hBox = new HBox(25);
-        hBox.getChildren().addAll(btn4,btn5,btn6);
 
-        root.getChildren().addAll(vBox, hBox);
+        // criar o borderpane
+        BorderPane borderPane = new BorderPane();
+
+        borderPane.setTop(btn1);
+        borderPane.setBottom(btn2);
+        borderPane.setLeft(btn3);
+        borderPane.setRight(btn4);
+        borderPane.setCenter(btn5);
+        
+        // aqui substitui o btn1 pelo btn 6
+        borderPane.setTop(btn6);
+
 
         // Criando uma cena com o layout
-        Scene scene = new Scene(root, 500, 500);
+        Scene scene = new Scene(borderPane, 500, 500);
 
         // configuração do Stage
         primaryStage.setTitle("Exemplo VBox e HBox");
@@ -44,4 +44,3 @@ public class VboxHboxExample extends Application{
         launch(args);
     }
 }    
-
